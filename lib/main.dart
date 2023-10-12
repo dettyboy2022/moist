125afbb539e9e0be67e4580147934a23611ae17f
dettyboy <dettyboy@icloud.com>
2023-10-12 12:26:06 +0100

    perf: :zap: used provider for the UI bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:moistwears/controllers/mainscreen_provider.dart';
 import 'package:moistwears/welcome.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (context) => MainScreenNotifier())
+  ], child: const MyApp()));
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
--- lib/presentation/view/bottomnavigation/bottomnav.dart
+++ lib/presentation/view/bottomnavigation/bottomnav.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:moistwears/controllers/mainscreen_provider.dart';
 import 'package:moistwears/presentation/view/cart/cart.dart';
 import 'package:moistwears/presentation/view/favourite/favourite.dart';
 import 'package:moistwears/presentation/view/profile/profile.dart';
+import 'package:provider/provider.dart';
 import '../homepage/home.dart';
 
 class BottomNav extends StatefulWidget {
@@ -16,46 +18,55 @@ class BottomNav extends StatefulWidget {
 class _BottomNavState extends State<BottomNav> {
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-        tabBar: CupertinoTabBar(
-            activeColor: Colors.black,
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(icon: Icon(Iconsax.home), label: 'Home'),
-              BottomNavigationBarItem(
-                  icon: Icon(Iconsax.heart), label: 'Favourite'),
-              BottomNavigationBarItem(
-                  icon: Icon(Iconsax.shopping_bag), label: 'Cart'),
-              BottomNavigationBarItem(
-                  icon: Icon(Iconsax.profile_2user), label: 'Profile'),
-            ]),
-        tabBuilder: (context, index) {
-          switch (index) {
-            case 0:
-              return CupertinoTabView(
-                builder: (context) {
-                  return const CupertinoPageScaffold(child: Home());
+    return Consumer<MainScreenNotifier>(
+      builder: (context, mainScreenNotifier, child) {
+        return CupertinoTabScaffold(
+            tabBar: CupertinoTabBar(
+                activeColor: Colors.black,
+                currentIndex: mainScreenNotifier.pageIndex,
+                onTap: (index) {
+                  mainScreenNotifier.setpageIndex(index);
                 },
-              );
-            case 1:
-              return CupertinoTabView(
-                builder: (context) {
-                  return const CupertinoPageScaffold(child: Favourite());
-                },
-              );
-            case 2:
-              return CupertinoTabView(
-                builder: (context) {
-                  return const CupertinoPageScaffold(child: Cart());
-                },
-              );
-            case 3:
-              return CupertinoTabView(
-                builder: (context) {
-                  return const CupertinoPageScaffold(child: Profile());
-                },
-              );
-          }
-          return Container();
-        });
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                      icon: Icon(Iconsax.home), label: 'Home'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Iconsax.heart), label: 'Favourite'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Iconsax.shopping_bag), label: 'Cart'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Iconsax.profile_2user), label: 'Profile'),
+                ]),
+            tabBuilder: (context, index) {
+              switch (mainScreenNotifier.pageIndex) {
+                case 0:
+                  return CupertinoTabView(
+                    builder: (context) {
+                      return const CupertinoPageScaffold(child: Home());
+                    },
+                  );
+                case 1:
+                  return CupertinoTabView(
+                    builder: (context) {
+                      return const CupertinoPageScaffold(child: Favourite());
+                    },
+                  );
+                case 2:
+                  return CupertinoTabView(
+                    builder: (context) {
+                      return const CupertinoPageScaffold(child: Cart());
+                    },
+                  );
+                case 3:
+                  return CupertinoTabView(
+                    builder: (context) {
+                      return const CupertinoPageScaffold(child: Profile());
+                    },
+                  );
+              }
+              return Container();
+            });
+      },
+    );
   }
 }
